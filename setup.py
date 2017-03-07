52aded43e23bdb54ec6f53e992fc5f4e3a81aae9
jdoherty7 <muons.quarks@gmail.com>
2017-03-07 11:13:16 -0600

    changed underscore in name to hyphen and added demo to package

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description' : 'Interpolation of a function \
-     and generates C code to quickly evaluate \
+    'description' : 'Used to adaptively interpolate a function \
+     then generates C code to quickly evaluate \
      the interpolation using pyopencl.',
     'author' : 'John Doherty',
     'author_email' : 'muons.quarks@gmail.com',
-    'version' : '0.2',
+    'version' : '0.3',
     'license' : 'MIT',
     'url' : 'https://www.github.com/jdoherty7/Adaptive_Interpolation',
     'install_requires': ['nose', 'numpy', 'matplotlib',
     'pyopencl', 'scipy', 'cgen'],
     'packages': ['adaptive_interpolation'],
-    'name': 'adaptive_interpolation'
+    'scripts': ['demo.py'],
+    'name': 'adaptive-interpolation'
 }
 
 
